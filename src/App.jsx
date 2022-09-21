ef0edbf6fa7fe7e2c970ca6732d061b0b6a3e750
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-09-22 05:05:00 +0700

    App.js updated by ethers integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,29 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
+import {Sidebar, Navbar} from './components';
+import {CreateCampaign, Home, Profile} from './pages';
 
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/main.jsx</code>, then modify the{" "}
-          <code className="code">src/App.jsx</code> file!
-        </p>
+const App = () => {
+  return(
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      {/* Sidebar Section */}
+      <div className='sm:flex hidden mr-10 relative'>
+        <Sidebar />
+      </div>
 
-        <div className="connect">
-          <ConnectWallet />
-        </div>
+      {/* Navbar section */}
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        <Navbar />
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/create-campaign' element={<CreateCampaign />}/>
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
+
+export default App
