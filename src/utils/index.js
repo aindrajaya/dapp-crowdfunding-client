5785eeea80eced3d47b9dba83ba84c708855ef6c
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-09-22 05:32:00 +0700

    add utils file
    Utilization file to add more function, such as check image, check percentage, days left etc

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+export const daysLeft = (deadline) => {
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainindDays = difference / (1000 * 3600 * 24);
+
+  return remainindDays.toFixed(0);
+}
+
+export const calculateBarPercentage = (goal, raisedAmount) => {
+  const percentage = Math.round((raisedAmount * 100) / goal);
+  return percentage;
+}
+
+export const checkIfImage = (url, callback) => {
+  const img = new Image();
+  img.src = url;
+
+  if(img.complete) callback(true);
+
+  img.onload = () => callback(true);
+  img.onerror = () => callback(false);
+}
