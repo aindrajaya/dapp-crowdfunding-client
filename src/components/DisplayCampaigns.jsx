c7241899558ac8ac9d2bdcc8b352448a8e34f384
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-10-02 05:51:00 +0700

    create DisplayCampaigns component
    create display campaigns component and add some filter on it

---
+++ src/components/DisplayCampaigns.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { loader } from '../assets';
+
+const DisplayCampaigns = ({title, isLoading, campaigns}) => {
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <h1 className='font-epilogue font-semibold text-[18px] text-white text-left'>{title} ({campaigns.length})</h1>
+      <div className='flex flex-wrap mt-[20px] gap-[26px]'>
+        {isLoading && (
+          <img src={loader} alt="loader" className='w-[100px] h-[100px] object-contain'/>
+        )}
+        
+        {!isLoading && campaigns.length === 0 && (
+          <p className='font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]'>
+            You have not created any campaigns yet
+          </p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default DisplayCampaigns
