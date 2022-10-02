7cc47d0b25f6cec066b0e6aafa1204bb330f5c0d
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-10-03 05:41:00 +0700

    update campaign card
    add campaign details from blockchain

--- src/components/CampaignCard.jsx
+++ src/components/CampaignCard.jsx
@@ -27,8 +27,13 @@ const CampaignCard = ({owner, title, description, target, deadline, amountCollec
 
         {/* blockchain content data */}
         <div className='flex justify-between flex-wrap mt-[15px] gap-2'> 
-          <div className=''>
-
+          <div className='flex flex-col'>
+            <h4 className='font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]'>{amountCollected}</h4>
+            <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate'>Raised of {target}</p>
+          </div>
+          <div className='flex flex-col'>
+            <h4 className='font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]'>{remaindingDays}</h4>
+            <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate'>Days Left</p>
           </div>
         </div>
       </div>
