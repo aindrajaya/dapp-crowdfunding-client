04ac85f3d925513bfdc722aecd1ad42348561cd7
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-10-03 06:01:00 +0700

    add campaign owner address

--- src/components/CampaignCard.jsx
+++ src/components/CampaignCard.jsx
@@ -36,6 +36,16 @@ const CampaignCard = ({owner, title, description, target, deadline, amountCollec
             <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate'>Days Left</p>
           </div>
         </div>
+
+        {/* Owner address */}
+        <div className='flex items-center mt-[20px] gap-[12px]'>
+          <div className='w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]'>
+            <img src={thirdweb} alt='user' className='w-1/2 h-1/2 object-contain'/>
+          </div>
+          <p className='flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate'>
+            by <span className='text-[#b2b3bd]'>{owner}</span>
+          </p>
+        </div>
       </div>
     </div>
   )
